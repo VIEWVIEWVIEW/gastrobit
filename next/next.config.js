54fc433bf43a1407a7dffe33be068a92bd693ede
VIEWVIEWVIEW <39219018+VIEWVIEWVIEW@users.noreply.github.com>
2023-04-10 20:41:52 +0200

    Fuck you eslint

--- next/next.config.js
+++ next/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    dirs: ['pages', 'utils', 'components', 'Layouts', 'types'], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
